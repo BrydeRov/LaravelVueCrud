a1f66edb481b0cfd7f0de7e8a8a06d8727b3b098
BrydeRov <andresjavieroksi@gmail.com>
2022-06-20 02:21:46 -0500

    first commit

---
+++ resources/js/Pages/User.vue
@@ -0,0 +1,21 @@
+<template>
+    <AppLayout>
+        <template #header>
+            <h2>User</h2>
+        </template>
+
+        <div>
+            <pre>
+                {{ users }}
+            </pre>
+        </div>
+    </Applayout>
+</template>
+
+<script>
+import AppLayout from '../../../vendor/laravel/jetstream/stubs/inertia/resources/js/Layouts/AppLayout.vue';
+export default{
+    props: ['users'],
+    components: { AppLayout }
+}
+</script>
